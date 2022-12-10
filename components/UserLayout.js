f77c33e0b7611b4743e5936ee4b92231073e4a3d
Iput <taufiqurrohman.suwarto@gmail.com>
2022-12-11 06:12:17 +0700

    lastcommit

--- components/UserLayout.js
+++ components/UserLayout.js
@@ -1,3 +1,40 @@
 // create layout for user using antd
 
 import React from "react";
+
+import { Layout, theme } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
+function UserLayout(props) {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible>Sider</Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          Header
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          {props.children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default UserLayout;
--- pages/user/index.js
+++ pages/user/index.js
@@ -2,26 +2,16 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../services/users.services";
 import { Skeleton, Typography } from "antd";
+import UserLayout from "../../components/UserLayout";
 
 function index() {
   const { data, isLoading } = useQuery(["users"], () => getUsers(), {});
   return (
-    <Skeleton loading={isLoading}>
-      <p>{JSON.stringify(data)}</p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore animi
-        aut consectetur fugit autem suscipit quia eligendi enim, reiciendis
-        architecto accusantium! Totam laudantium recusandae architecto illum
-        asperiores culpa a eius.
-      </p>
-
-      <Typography.Text>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
-        cumque quia quod accusantium dolores. Distinctio fuga architecto nulla
-        ipsam aliquid debitis atque dicta harum. Ex nulla modi nemo impedit
-        reprehenderit.
-      </Typography.Text>
-    </Skeleton>
+    <UserLayout>
+      <div>
+        <Typography.Title level={2}>Users</Typography.Title>
+      </div>
+    </UserLayout>
   );
 }
 
