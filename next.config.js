f927794fd451a8e5ec217885c59517fa3f8e3a55
Iput <taufiqurrohman.suwarto@gmail.com>
2022-12-11 14:39:26 +0700

    last commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
+const isProd = process.env.NODE_ENV === "production";
+
+function getBasePath() {
+  var basePath = "";
+
+  if (isProd && process.env.BASE_PATH) {
+    if (process.env.BASE_PATH.startsWith("/")) {
+      basePath = process.env.BASE_PATH;
+    } else {
+      basePath = "/" + process.env.BASE_PATH;
+    }
+  }
+
+  return basePath;
+}
+
 const nextConfig = {
-  reactStricMode: true,
+  images: {
+    domains: ["siasn.bkd.jatimprov.go.id"],
+    formats: ["image/webp"],
+  },
+  poweredByHeader: false,
+  basePath: "/esign",
+  publicRuntimeConfig: {
+    basePath: getBasePath(),
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { SessionProvider } from "next-auth/react";
 import "antd/dist/reset.css";
 import { useState } from "react";
 import {
@@ -6,17 +5,31 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { SessionProvider, signIn, useSession } from "next-auth/react";
+
+const Auth = ({ children }) => {
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated: () => signIn(),
+  });
+
+  if (status === "loading") return null;
+
+  return children;
+};
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydrateState}>
-        <SessionProvider>
-          <Component {...pageProps} />
-        </SessionProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <SessionProvider session={session} baseUrl="/esign" basePath="/esign/auth">
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydrateState}>
+          <SessionProvider>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
 
