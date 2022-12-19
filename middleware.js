ef6002703537cb445a688f3df20e271c60112c29
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-12-19 16:29:03 +0700

    last commit

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { withAuth } from "next-auth/middleware";
+
+// More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      console.log("yeah");
+      // `/admin` requires admin role
+      if (req.nextUrl.pathname === "/admin") {
+        return token?.userRole === "admin";
+      }
+      // `/me` only requires the user to be logged in
+      return !!token;
+    },
+  },
+});
+
+export const config = { matcher: ["/admin", "/me"] };
