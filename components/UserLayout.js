63a7b2375a4029faa528b3fe2be511c53ced598c
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-12-27 13:39:25 +0700

    add layout for users

--- components/UserLayout.js
+++ components/UserLayout.js
@@ -2,9 +2,22 @@ import {
   DashboardOutlined,
   FileOutlined,
   LogoutOutlined,
+  PlusOutlined,
   SettingOutlined,
+  UploadOutlined,
 } from "@ant-design/icons/lib/icons";
-import { Avatar, Dropdown, Layout, Menu, Space, theme, Typography } from "antd";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  Layout,
+  Menu,
+  Modal,
+  Space,
+  theme,
+  Tooltip,
+  Typography,
+} from "antd";
 import { useRouter } from "next/router";
 import { useState } from "react";
 const { Header, Content, Sider } = Layout;
@@ -17,6 +30,56 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
+
+const UploadModal = ({ open, onCancel }) => {
+  const router = useRouter();
+
+  const listButton = [
+    {
+      id: "selfSign",
+      title: "Self Sign",
+      helpText:
+        "Sign document. You can store, download and send it through email, cloud storage, or other means.",
+    },
+    {
+      id: "signAndRequest",
+      title: "Sign and Request",
+      helpText:
+        "Sign document and request others to sign it. You can store, download and send it through email, cloud storage, or other means.",
+    },
+    {
+      id: "requestFromOthers",
+      title: "Request from others",
+      helpText:
+        "Request others to sign document. You can store, download and send it through email, cloud storage, or other means.",
+    },
+  ];
+
+  return (
+    <Modal
+      centered
+      footer={null}
+      title="Choose Document Workflow"
+      open={open}
+      onCancel={onCancel}
+    >
+      <Space>
+        {listButton.map((item) => (
+          <Tooltip key={item.id} title={item?.helpText} placement="bottom">
+            <Button
+              type="primary"
+              onClick={() => {
+                router.push(`/user/documents/${item.id}`);
+              }}
+            >
+              {item.title}
+            </Button>
+          </Tooltip>
+        ))}
+      </Space>
+    </Modal>
+  );
+};
 
 const drawItems = [
   {
@@ -62,6 +125,11 @@ const UserLayout = ({ children, active = "/user/dashboard" }) => {
   };
 
   const [collapsed, setCollapsed] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  const handleCancel = () => setOpen(false);
+  const showModal = () => setOpen(true);
+
   const {
     token: { colorBgContainer },
   } = theme.useToken();
@@ -77,13 +145,31 @@ const UserLayout = ({ children, active = "/user/dashboard" }) => {
         defaultCollapsed={true}
         onCollapse={(value) => setCollapsed(value)}
       >
-        <div
-          style={{
-            height: 32,
-            margin: 16,
-            background: "rgba(255, 255, 255, 0.2)",
-          }}
-        />
+        {!collapsed ? (
+          <div style={{ padding: "18px" }}>
+            <Button
+              danger
+              type="primary"
+              onClick={showModal}
+              icon={<UploadOutlined />}
+              block
+            >
+              Upload
+            </Button>
+          </div>
+        ) : (
+          <Button
+            style={{
+              // middleware
+              margin: "18px",
+            }}
+            onClick={showModal}
+            type="primary"
+            danger
+            icon={<PlusOutlined />}
+          />
+        )}
+        <UploadModal open={open} onCancel={handleCancel} />
         <Menu
           theme="dark"
           defaultSelectedKeys={[active]}
