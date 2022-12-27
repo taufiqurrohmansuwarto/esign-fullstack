93168456e6dbad46f2cff45bc35cad16664266ec
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-12-27 16:57:55 +0700

    add pdf utils to footer

---
+++ middlewares/footer.middleware.js
@@ -0,0 +1,9 @@
+// this middleware will create you automate footer for you documents
+const { PDFDocument, StandardFonts, rgb, degrees } = require("pdf-lib");
+const fs = require("fs");
+const { times } = require("lodash");
+const path = require("path");
+
+const footer = async (documentId) => {};
+
+module.exports = footer;
