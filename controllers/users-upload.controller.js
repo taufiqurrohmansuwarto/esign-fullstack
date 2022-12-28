248bf888070af05e636acffebb424df58e2787a9
Iput <taufiqurrohman.suwarto@gmail.com>
2022-12-28 19:04:42 +0700

    last commit

--- controllers/users-upload.controller.js
+++ controllers/users-upload.controller.js
@@ -1,4 +1,17 @@
-const documentUpload = async (req, res) => {};
+const documentUpload = async (req, res) => {
+  try {
+    const { file } = req;
+    console.log(file);
+    res.json({
+      message: "Upload Success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
 
 module.exports = {
   documentUpload,
--- pages/api/user/upload.js
+++ pages/api/user/upload.js
@@ -1,5 +1,14 @@
 import { createRouter } from "next-connect";
+import multer from "multer";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 const router = createRouter();
 
-router.use();
+router.use().post(multer().single("document"));
+
+export default router.handler();
