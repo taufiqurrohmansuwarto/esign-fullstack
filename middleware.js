6a4efa06683fee687484d82fe16bcad7f5fe2e72
Iput <taufiqurrohman.suwarto@gmail.com>
2022-12-29 20:55:31 +0700

    last commit

--- middleware.js
+++ middleware.js
@@ -1,18 +1,13 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    console.log("test");
-  },
-  {
-    callbacks: {
-      authorized({ req, token }) {
-        console.log(token);
-        return !!token;
-      },
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized({ req, token }) {
+      console.log(token);
+      return !!token;
     },
-  }
-);
+  },
+});
 
 export const config = {
   matcher: ["/testing"],
