1e9a842003c24dc266f17e8a012ad1a66caab5a7
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-01-24 17:23:39 +0700

    last commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const isProd = process.env.NODE_ENV === "production";
 
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
+// if (
+//   process.env.LD_LIBRARY_PATH == null ||
+//   !process.env.LD_LIBRARY_PATH.includes(
+//     `${process.env.PWD}/node_modules/canvas/build/Release:`
+//   )
+// ) {
+//   process.env.LD_LIBRARY_PATH = `${
+//     process.env.PWD
+//   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+// }
 
 function getBasePath() {
   var basePath = "";
