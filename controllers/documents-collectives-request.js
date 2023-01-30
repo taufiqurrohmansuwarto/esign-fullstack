d2e72390d590fffd8d36f972afeb408dcd254722
Iput <taufiqurrohman.suwarto@gmail.com>
2023-01-31 03:21:30 +0700

    database document collective

---
+++ controllers/documents-collectives-request.js
@@ -0,0 +1,32 @@
+const index = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const update = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const remove = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const create = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const show = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  index,
+  update,
+  remove,
+  create,
+  show,
+};
